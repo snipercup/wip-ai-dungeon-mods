8abbbc776f87cc8ae4399dce3f007c9656c2ed01
Taleir of Deynai <TaleirOfDeynai@live.com>
2021-03-21 14:51:29 -0700

    Replace `report-game-info` command with something more generic.

--- src/index.js
+++ src/index.js
@@ -10,10 +10,22 @@ const annotatedMode = require("./annotated-context-mode");
 const pipeline = new Pipeline();
 
 pipeline.commandHandler.addCommand(new SimpleCommand(
-  "report-game-info",
+  "report-global",
+  (data, [name]) => {
+    if (name in globalThis) {
+      // @ts-ignore - We checked, dammit!
+      console.log(globalThis[name]);
+      return `Global variable \`${name}\` dumped to logs.`;
+    }
+    return `The global variable \`${name}\` did not exist.`
+  })
+);
+
+pipeline.commandHandler.addCommand(new SimpleCommand(
+  "report-global-vars",
   (data) => {
-    console.log(data.info);
-    return "Game info dumped to logs.";
+    console.log(Object.keys(globalThis));
+    return "Global variable names dumped to logs.";
   })
 );
 

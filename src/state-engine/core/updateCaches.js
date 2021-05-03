7aed524c72aefabd895f0c884652ff8c5ca7b8da
Taleir of Deynai <TaleirOfDeynai@live.com>
2021-05-03 07:59:14 -0700

    TS dumb.

--- src/state-engine/core/updateCaches.js
+++ src/state-engine/core/updateCaches.js
@@ -53,6 +53,7 @@ module.exports = (data) => {
   ctx.theCache.commit();
 
   // Finally, update the parsed entry cache and we're done!
+  // @ts-ignore - Why are you bothering with this, TS?  Stupid!
   state.$$stateDataCache = chain(toPairs(ctx.entriesMap))
     .map(([k, entry]) => [k, entry.toJSON()])
     .value((kvps) => fromPairs(kvps));
